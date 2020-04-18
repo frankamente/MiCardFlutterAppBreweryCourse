905fc64a8f14724791f5957e8e31c11c2c856c45
frankamente <fran29400@gmail.com>
2020-04-18 17:17:48 +0200

    First Approach

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,129 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var primaryColor = Colors.teal[700];
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        backgroundColor: primaryColor,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 100.0,
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/personal_picture.jpg"),
+                backgroundColor: Colors.transparent,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    width: 100.0,
-                    height: 100.0,
-                  ),
-                ],
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Fran Márquez",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32.0,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  color: Colors.teal[100].withAlpha(230),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Rock Salt",
+                ),
+              ),
+              SizedBox(
+                height: 10,
               ),
               Container(
-                color: Colors.blue,
-                width: 100.0,
+                color: Colors.teal[100].withAlpha(230),
+                height: 1,
+                margin: EdgeInsets.symmetric(horizontal: 160),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextCard(
+                icon: Icon(
+                  Icons.phone,
+                  color: primaryColor,
+                ),
+                text: Text(
+                  "+34 605 371 174",
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextCard(
+                icon: Icon(
+                  Icons.mail,
+                  color: primaryColor,
+                ),
+                text: Text(
+                  "fran29400@gmail.com",
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
               ),
             ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class TextCard extends StatelessWidget {
+  const TextCard({
+    Key key,
+    @required this.icon,
+    @required this.text,
+  }) : super(key: key);
+
+  final Icon icon;
+  final Text text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      margin: EdgeInsets.symmetric(horizontal: 50),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(3),
+      ),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 12),
+            child: icon,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50),
+            child: text,
+          )
+        ],
+      ),
+    );
+  }
 }
